59e88a639228c59eb7fa26f015a7c602ddbf7244
Łukasz Tenerowicz <konkit@gmail.com>
2015-10-31 15:11:46 +0100

    Working project

---
+++ app.js
@@ -0,0 +1,111 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var util = require('util');
+
+Player = require("./Player").Player;	// Player class
+
+// Serving index.html as default
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+// Serving static files
+app.use('/public', express.static('public'));
+
+var players = [];	// Array of connected players
+
+io.on('connection', onSocketConnection);
+
+// New socket connection
+function onSocketConnection(client) {
+	util.log("New player has connected: "+client.id);
+
+	// Listen for client disconnected
+	client.on("disconnect", onClientDisconnect);
+
+	// Listen for new player message
+	client.on("new player", onNewPlayer);
+
+	// Listen for move player message
+	client.on("move player", onMovePlayer);
+};
+
+// Socket client has disconnected
+function onClientDisconnect() {
+	util.log("Player has disconnected: "+this.id);
+
+	var removedPlayer = playerById(this.id);
+
+	// Player not found
+	if (!removedPlayer) {
+		util.log("Player not found: "+this.id);
+		return;
+	};
+
+	// Remove player from players array
+	players.splice(players.indexOf(removedPlayer), 1);
+
+	// Broadcast removed player to connected socket clients
+	this.broadcast.emit("removing player", {id: this.id});
+};
+
+// New player has joined
+function onNewPlayer(data) {
+	// Create a new player
+	var newPlayer = new Player(data.x, data.y);
+	newPlayer.id = this.id;
+
+	// Broadcast new player to connected socket clients
+	this.broadcast.emit("new player", {id: newPlayer.id, x: newPlayer.getX(), y: newPlayer.getY()});
+
+	// Send existing players to the new player
+	var i, existingPlayer;
+	for (i = 0; i < players.length; i++) {
+		existingPlayer = players[i];
+		this.emit("new player", {id: existingPlayer.id, x: existingPlayer.getX(), y: existingPlayer.getY()});
+	};
+
+	// Add new player to the players array
+	players.push(newPlayer);
+};
+
+// Player has moved
+function onMovePlayer(data) {
+	// Find player in array
+	var movePlayer = playerById(this.id);
+
+	// Player not found
+	if (!movePlayer) {
+		util.log("Player not found: "+this.id);
+		return;
+	};
+
+	// Update player position
+	movePlayer.setX(data.x);
+	movePlayer.setY(data.y);
+
+	// Broadcast updated position to connected socket clients
+	this.broadcast.emit("move player", {id: movePlayer.id, x: movePlayer.getX(), y: movePlayer.getY()});
+};
+
+
+/**************************************************
+** GAME HELPER FUNCTIONS
+**************************************************/
+// Find player by ID
+function playerById(id) {
+	var i;
+	for (i = 0; i < players.length; i++) {
+		if (players[i].id == id)
+			return players[i];
+	};
+
+	return false;
+};
+
+// Start Express.js server
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
