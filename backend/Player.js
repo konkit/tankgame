66ba1189ad469acc50321e6245b4452af381b60d
Tomasz Pachana <tomasz.pachana@sensinum.pl>
2015-11-28 12:46:35 +0100

    player fixup

--- backend/Player.js
+++ backend/Player.js
@@ -4,41 +4,41 @@ class Player {
   constructor(id, position) {
     this._id = id;
     this._position = position;
-		this._missiles = [];
+    this._missiles = [];
   }
 
-	shotSucceeded(missile) {
-		let idx = this._missiles.indexOf(missile);
-		this._missiles.splice(index, 0);
-		// some points or sth may be admitted here
-	}
+  shotSucceeded(missile) {
+    let idx = this._missiles.indexOf(missile);
+    this._missiles.splice(index, 1);
+    // some points or sth may be admitted here
+  }
 
-	updateMissilesPos(missileLostCallback) {
-			for (let missile of this._missiles) {
-				let missileAvailable = missile.updatePosition();
-				if (!missileAvailable) {
-					let idx = this._missiles.indexOf(missile);
-					this._missiles.slice(idx, 0);
-					missileLostCallback(missile);
-				}
-			}
-	}
+  updateMissilesPos(missileLostCallback) {
+    for (let missile of this._missiles) {
+      let missileAvailable = missile.updatePosition();
+      if (!missileAvailable) {
+        let idx = this._missiles.indexOf(missile);
+        this._missiles.splice(idx, 1);
+        missileLostCallback(missile);
+      }
+    }
+  }
 
-	get id() {
-		return this._id;
-	}
+  get id() {
+    return this._id;
+  }
 
-	get position() {
-		return this._position;
-	}
+  get position() {
+    return this._position;
+  }
 
-	set position(newPos) {
-		this._position = newPos;
-	}
+  set position(newPos) {
+    this._position = newPos;
+  }
 
-	get missiles() {
-		return this._missiles;
-	}
+  get missiles() {
+    return this._missiles;
+  }
 }
 
 module.exports = Player;
